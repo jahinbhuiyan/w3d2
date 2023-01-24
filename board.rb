e3b0d314f2b7794eda785f2ae60504ea2773a1aa
Kevin Chan <chankevin13@gmail.com>
2023-01-24 14:53:20 -0500

    board2

--- board.rb
+++ board.rb
@@ -10,4 +10,15 @@ class Board
         @grid = Array.new(n) { Array.new(n, " ") }
         @size = n * n
       end
+
+      def [](pos)
+        row, col = pos
+        @grid[row][col]
+      end
+    
+      def []=(pos, val)
+        row, col = pos
+        @grid[row][col] = val
+      end
+      
 end
