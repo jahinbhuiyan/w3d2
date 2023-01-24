516813d978aadd16a0b89c183452a9d8c3b94289
jahinbhuiyan <jahinbarobhuiyan@gmail.com>
2023-01-24 14:26:32 -0500

    card

---
+++ card.rb
@@ -0,0 +1,18 @@
+class Card
+
+    
+    def initialize(value)
+        @value = value
+        @face_up = false
+    end
+
+    def hide
+        @face_up = false
+    end
+
+    def reveal
+        @face_up = true
+    end
+
+    
+end
